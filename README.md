5b65ede27b7542cde7a80c244a0805a5d8fe67bd
Stepan Zhevak <enet@protonmail.ch>
2016-01-10 10:39:35 +0300

    Cache implementation was changed

--- README.md
+++ README.md
@@ -1,15 +1,11 @@
 # gobem-proc-prettydiff
-This processor for [gobem](https://github.com/Enet/gobem) minifies files using **prettydiff**. All options are passed as a single object. Empty files are just skipped during processing. **gobem-proc-prettydiff** requires redis database to cache results of the work.
+This processor for [gobem](https://github.com/Enet/gobem) minifies files using **prettydiff**. All options are passed as a single object. Empty files are just skipped during processing. **gobem-proc-prettydiff** requires directory to cache results of the work.
 
 The following options are supported:
 * `ignoreErrors`<br>
 If this flag is `true` and error occured, raw file's content will be written instead minified one.
-* `redisKey`<br>
-The key in the redis database to store cache. Default value is `gobem-proc-prettydiff`.
-* `redisClient`<br>
-Already created redis-client. [This](https://github.com/NodeRedis/node_redis) module is used.
-* `redisOptions`<br>
-Options for a new redis-client. This field is ignored, if `redisClient` is passed.
+* `cacheDir`<br>
+Full path to a readable and writable directory to cache files.
 
 Also there is one useful feature: if you don't want to minify some specific file, just add one of the following lines into any place:
 * /* prevent prettydiff */
--- index.js
+++ index.js
@@ -1,48 +1,40 @@
 'use strict';
 
-var prettydiff = require('prettydiff'),
-    redis = require('redis'),
+var path = require('path'),
+    fs = require('fs'),
+    prettydiff = require('prettydiff'),
+    xxhash = require('xxhash'),
     re = /^\s*\/?\/?(\*|'|"|)\s*prevent\sprettydiff\s*\1\/?;?\s*$/m;
 
 module.exports = function (options) {
     options = options || {};
 
-    let client = options.redisClient,
-        key = options.redisKey || 'gobem-proc-prettydiff';
-
     return {
-        before: function (next) {
-            client = client || redis.createClient(options.redisOptions);
-            client.expire(key, 86400);
-            next();
-        },
+        process: function (next, input, output, config, rawContent, rawPath) {
+            if (!rawContent) return next();
 
-        process: function (next, input, output, config, content, path) {
-            if (!content) return next();
-            client.hget(key, content, function (error, reply) {
-                if (reply === null) {
+            let key = xxhash.hash(new Buffer(rawContent), 0xCAFEBABE) + '',
+                filePath = path.join(options.cacheDir + '', 'gobem-proc-prettydiff^' + key);
+
+            fs.readFile(filePath, 'utf8', (error, fileContent) => {
+                if (error) {
                     try {
-                        output.set(path, re.test(content) ? content : prettydiff.api({
+                        output.set(rawPath, re.test(rawContent) ? rawContent : prettydiff.api({
                             mode: 'minify',
                             lang: 'auto',
                             report: false,
-                            source: content
+                            source: rawContent
                         })[0]);
-                        client.hset(key, content, output.get(path), next);
+                        fs.writeFile(filePath, output.get(rawPath), next);
                     } catch (error) {
-                        output.set(path, content);
+                        output.set(rawPath, rawContent);
                         next(options.ignoreErrors ? null : error);
                     }
                 } else {
-                    output.set(path, reply);
+                    output.set(rawPath, fileContent);
                     next();
                 }
             });
-        },
-
-        clear: function (next) {
-            !options.redisClient && client.end();
-            next();
         }
     };
 };
